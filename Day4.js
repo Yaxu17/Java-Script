bda81eb54169db9b74d79439dab710c7c3d9366d
Yaxu <yokshitrajora1@gmail.com>
2026-01-27 15:02:47 +0530

    day5

---
+++ Day4.js
@@ -0,0 +1,20 @@
+// console.log(a)
+// console.log(b)
+
+
+// let a = 567324
+// var b = 7584
+
+// console.log(a)
+// console.log(b)
+
+// function print(){
+//     console.log("Hello")
+// }
+// print()
+
+// console.log("line")
+// setTimeout(() => {
+//     console.log('fedwkbjn')
+    
+// }, 2000);
