66d95646c469a1be3b39c70a9c69ec046592b633
Yaxu <yokshitrajora1@gmail.com>
2026-02-04 11:46:27 +0530

    another day

--- Day4.js
+++ Day4.js
@@ -18,3 +18,37 @@
 //     console.log('fedwkbjn')
     
 // }, 2000);
+
+// console.log('starting homework....')
+
+// setTimeout(() => {
+//     console.log("Homework Done!");
+//     console.log("Starting Dinner...");
+
+//     setTimeout(() => {
+//        console.log("Dinner Done!");
+//        console.log("Going to sleep....");
+       
+//        setTimeout(() => {
+//         console.log("playing....");
+//        },1000);
+//     }, 1500);
+// }, 2000);
+
+const p=new Promise((res,rej)=>{
+    let done = true
+    setTimeout(()=>{
+        if (done){
+            res({name: "Anand", age: 23})
+        }else{
+            rej("Word has not been completed")
+        }
+    },5000)
+})
+p.then((data)=>{
+    console.log(data.name)
+}).catch((err)=>{
+    console.log(err)
+}).finally(()=>{
+    console.log("finally block")
+})
