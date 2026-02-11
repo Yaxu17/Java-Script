61749971380af921ab34ab2273cdfa36325cd25d
Yaxu <yokshitrajora1@gmail.com>
2026-02-11 22:41:29 +0530

    New Day

--- Day4.js
+++ Day4.js
@@ -35,20 +35,26 @@
 //     }, 1500);
 // }, 2000);
 
-const p=new Promise((res,rej)=>{
-    let done = true
-    setTimeout(()=>{
-        if (done){
-            res({name: "Anand", age: 23})
-        }else{
-            rej("Word has not been completed")
-        }
-    },5000)
-})
-p.then((data)=>{
-    console.log(data.name)
-}).catch((err)=>{
-    console.log(err)
-}).finally(()=>{
-    console.log("finally block")
-})
+// const p=new Promise((res,rej)=>{
+//     let done = true
+//     setTimeout(()=>{
+//         if (done){
+//             res({name: "Anand", age: 23})
+//         }else{
+//             rej("Word has not been completed")
+//         }
+//     },5000)
+// })
+// p.then((data)=>{
+//     console.log(data.name)
+// }).catch((err)=>{
+//     console.log(err)
+// }).finally(()=>{
+//     console.log("finally block")
+// })
+
+function doHomework(){
+    const p =new Promise((res,rej)=>{
+        let done=true
+    })
+}
