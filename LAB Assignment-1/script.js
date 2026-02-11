6fecb8cd22ca59aa40be79ad6702a16e14444001
Yaxu <yokshitrajora1@gmail.com>
2026-02-11 16:25:12 +0530

    Dashboard Made

--- LAB Assignment-1/script.js
+++ LAB Assignment-1/script.js
@@ -5,4 +5,34 @@ form.addEventListener("submit", function(event) {
     console.log(eventDate.value);
     console.log(category.value);
     console.log(description.value);
+
+    
 });
+
+const eventCards = document.querySelector(".cards")
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    const title = eventTitle.value
+    const date = eventDate.value
+    const cat = category.value
+    const desc = description.value
+
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `
+        <h2>${title}</h2>
+        <p>Date:📅 ${date}</p>
+        <button class="category">${cat}</button>
+        <p>Description: ${desc}</p>
+        <div class="deleteCard">X</div>
+    `
+
+    card.querySelector(".deleteCard").addEventListener("click", function() {
+        card.remove()
+    })
+    eventCards.appendChild(card)
+})
+
+document.addEventListener("keydown", function(event) {
+    document.querySelector('.key').innerText = event.key
+})
